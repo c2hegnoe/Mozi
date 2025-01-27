756922726f17d2dadadb0b6091d859264a9ecc93
c2hegnoe <c2hegnoe@vasvari.org>
2025-01-27 13:13:23 +0100

    Alap függvények

---
+++ fapados_mozi_feladat.py
@@ -0,0 +1,27 @@
+import random
+
+nezoter = []
+nezoter_sor = []
+
+felnottar = 2500 
+diakar = 2100
+gyerekar = 1300
+ures = 0
+
+
+def nezoter_ulesrend():
+
+    for i in range(1):
+        nezoter_sor.append(random.randint(0,3))
+        
+    for n in range(1):
+        nezoter_sor.append("III")
+
+    for n in range(10):
+        nezoter_sor.append(random.randint(0,3))
+
+
+
+        
+        
+
