443fa3b7ec0445ed18fef0f6311489393af144b6
c2hegnoe <c2hegnoe@vasvari.org>
2025-01-30 10:57:19 +0100

    nezoter feltoltes

--- fapados_mozi_feladat.py
+++ fapados_mozi_feladat.py
@@ -1,27 +1,17 @@
 import random
 
 nezoter = []
-nezoter_sor = []
 
-felnottar = 2500 
-diakar = 2100
-gyerekar = 1300
-ures = 0
+def nezoter_feltoltes():
+    for i in range(15):
 
+        for n in range(10):
+            nezoter.append(random.randint(0, 3))
 
-def nezoter_ulesrend():
+        nezoter.append(' | ')  
 
-    for i in range(1):
-        nezoter_sor.append(random.randint(0,3))
-        
-    for n in range(1):
-        nezoter_sor.append("III")
+        for n in range(10):
+            nezoter.append(random.randint(0, 3))
 
-    for n in range(10):
-        nezoter_sor.append(random.randint(0,3))
-
-
-
-        
-        
+    print(nezoter)
 
