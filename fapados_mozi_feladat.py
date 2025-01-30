3572ef3e9e34d62f52e03b6dd39b83d2c87a2523
c2hegnoe <c2hegnoe@vasvari.org>
2025-01-30 11:06:26 +0100

    arak,bevetel

--- fapados_mozi_feladat.py
+++ fapados_mozi_feladat.py
@@ -15,4 +15,22 @@ def nezoter_feltoltes():
 
     print(nezoter)
 
-print("Hello")
+
+def bevetel(a):
+
+    osszeg = 0
+    felnott_ar = 2500
+    diak_nyugdijas_ar = 2100
+    gyerek_ar = 1300
+    szabad_hely_ar = 0
+
+    for i in range(len(nezoter)):
+        
+        if a == 0:
+            osszeg += felnott_ar
+        if a == 1:
+            osszeg += diak_nyugdijas_ar
+        if a == 2:
+            osszeg += gyerek_ar
+        if a == 3:
+            osszeg += szabad_hely_ar
