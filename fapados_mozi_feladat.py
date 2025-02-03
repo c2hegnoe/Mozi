c562882277df7de7a04fd5e54224b8f522d04f78
c2hegnoe <c2hegnoe@vasvari.org>
2025-02-03 09:37:22 +0100

    felnott jegyek,telitettseg

--- fapados_mozi_feladat.py
+++ fapados_mozi_feladat.py
@@ -1,19 +1,22 @@
 import random
 
-nezoter = []
+Nezoter_lista = []
 
 def nezoter_feltoltes():
     for i in range(15):
+        sor = []
 
         for n in range(10):
-            nezoter.append(random.randint(0, 3))
+            sor.append(random.randint(0, 3))
 
-        nezoter.append(' | ')  
+        sor.append(' | ')  
 
         for n in range(10):
-            nezoter.append(random.randint(0, 3))
+            sor.append(random.randint(0, 3))
 
-    print(nezoter)
+        print(sor)
+
+        Nezoter_lista.append(sor)
 
 
 def bevetel(a):
@@ -24,13 +27,45 @@ def bevetel(a):
     gyerek_ar = 1300
     szabad_hely_ar = 0
 
+    for i in range(len(Nezoter_lista)):
+        for a in range(len(Nezoter_lista[i])):
+
+            if Nezoter_lista[i][a] == 0:
+                osszeg += felnott_ar
+
+            if Nezoter_lista[i][a] == 1:
+                osszeg += diak_nyugdijas_ar
+
+            if Nezoter_lista[i][a] == 2:
+                osszeg += gyerek_ar
+
+            if Nezoter_lista[i][a] == 3:
+                osszeg += szabad_hely_ar
+
+
+        print("Összes bevétel összege: ",osszeg, "Ft")
+
+
+
+def telitettseg(nezoter):
+    foglalt_ulohelyek = 300
+
     for i in range(len(nezoter)):
-        
-        if a == 0:
-            osszeg += felnott_ar
-        if a == 1:
-            osszeg += diak_nyugdijas_ar
-        if a == 2:
-            osszeg += gyerek_ar
-        if a == 3:
-            osszeg += szabad_hely_ar
+        for a in range(len(nezoter[i])):
+            if nezoter [i][a] == 0:
+                foglalt_ulohelyek -= 1
+
+
+def felnott_jegyek(nezoter):
+    felnott_jegyek_szama = 0
+
+    for i in nezoter:
+        for a in i:
+            if a == 3:
+                felnott_jegyek_szama += 1
+    print(felnott_jegyek_szama)
+
+
+nezoter_feltoltes()
+felnott_jegyek(Nezoter_lista)
+
